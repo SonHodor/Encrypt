afcedbb1cd7c14f54b141498f64c09a00345cd0d
SonHodor <6164908@gmail.com>
2018-08-15 23:02:03 +0300

    add some func

--- main.cpp
+++ main.cpp
@@ -2,12 +2,22 @@
 
 using namespace std;
 
+
+
+void pisos(int k)
+	{
+		for (int i = 0; i < k; ++i)
+		{
+			cout<<"pisos"<<endl;
+		}
+	}
+
 int main()
 {
     cout << "Hello world!" << endl;
     int k = 0;
     //privet mama papa babushka ii kot
     cin>>k;
-    cout<<k<<endl;
+    pisos(k);
     return 0;
 }
