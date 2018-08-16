7ade2103d5bda916e3499edc3142b1fbbae53204
SonHodor <6164908@gmail.com>
2018-08-16 23:08:52 +0300

    becoming kindof program

--- main.cpp
+++ main.cpp
@@ -4,20 +4,29 @@ using namespace std;
 
 
 
-void pisos(int k)
-	{
-		for (int i = 0; i < k; ++i)
-		{
+void pisos(int k){
+		for (int i = 0; i < k; ++i){
 			cout<<"pisos"<<endl;
-		}
 	}
+}
 
-int main()
-{
-    cout << "Hello world!" << endl;
+int numx(int k){
+	return k*k;
+}
+
+
+int main(){
     int k = 0;
     //privet mama papa babushka ii kot
     cin>>k;
-    pisos(k);
+    for (int i = 0; i <= k ; ++i)
+    {
+    	if (i+1>k){
+    		pisos(1);
+    	}else{
+    	cout << numx(i+1)<<endl;
+    	}
+
+    }
     return 0;
 }
