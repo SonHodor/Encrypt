bd065a19504d5ba7397e5f596e1ec19ae9ddd871
SonHodor <6164908@gmail.com>
2018-10-13 21:41:34 +0300

    Class Encrypt added

--- Encrypt.cpp
+++ Encrypt.cpp
@@ -1,39 +1,35 @@
 #include "std_lib_facilities.h"
 //
-//Hi! Half of this code doesn't work how it must
-//but..
-//It's just First commit!
+//Program was absolutly re-writed, I mooved all encrypting actions from *main* to *encrypt* class
+//
 
 
 
-//D:\Programming\Ready-C++\SomeBadProject
 
 
 
-class letMess
+
+class letter
 {
 public:
     char letCh;
     int letKey;
     char letCh5B[5];
-    letMess(char ch)
+    letter(char ch)
         :letCh(ch){}
-    letMess()
-        :letCh('a'){}
+    letter();
 
-
-
-
-    void keyTo5bArr(){
+    void keyTo5bArr()
+    {
         string beconAB = "aaaaabbbbbabbbaabbababbaaababaabaaaaa";
         for (int i = 0; i < 5; ++i)
         {
             letCh5B[i] = beconAB[letKey+i];
         }
-
     }
 
-    void charToBeconKey(){
+    void charToBeconKey()
+    {
         if((letCh>96)&&(letCh<123)){
 
         letKey = binSearchLetter("abcdefghijklmnopqrstuvwxyz",0,26,letCh)-97;
@@ -43,18 +39,16 @@ public:
         }
     }
 
-    void printArr(){
+    void printArr()
+    {
         for (int i = 0; i < 5; ++i)
         {
             cout<<letCh5B[i]<<' ';
         }
         cout<<endl;
     }
-
-
-
-
 private:
+
     int binSearchLetter(string str, int l, int r, char x){
         int help = l+(r-l)/2;
         if (str[help]==x)
@@ -86,6 +80,41 @@ private:
 };
 
 
+class encrypt
+{
+public:
+    string mess;
+    int g;
+    letter message[];
+
+    encrypt(string m)
+        :mess(m){
+        	
+        }
+    encrypt();
+
+    void fillArr(){
+        for (int i = 0; i < mess.length(); ++i)
+        {
+            message[i].letCh = mess[i];
+            message[i].charToBeconKey();
+            message[i].keyTo5bArr();
+        }
+    }
+
+    void showMess(){
+        for (int i = 0; i < mess.length(); ++i)
+        {
+            message[i].printArr();
+        }
+
+    }
+};
+
+
+
+
+
 
 
 int main()
@@ -94,16 +123,11 @@ int main()
 
     string mess;
     getline(cin,mess);
-    int g = mess.length();
 
-    letMess k[g];
-    for (int i = 0; i < g; ++i)
-    {
-        k[i].letCh = mess[i];
-        k[i].charToBeconKey();
-        k[i].keyTo5bArr();
-        k[i].printArr();
-    }
+    encrypt messageEnc(mess);
+    messageEnc.fillArr();
+    messageEnc.showMess();
+    cout<<"kek"<<endl;
 
 
 
